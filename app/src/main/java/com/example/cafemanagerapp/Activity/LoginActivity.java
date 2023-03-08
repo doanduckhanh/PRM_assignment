c53f5b56e2f32dda4aa76faa250d305018201a22
Nguyễn Tiến Nhất <nhatnthe153132@fpt.edu.vn>
2023-03-08 10:10:35 +0700

    update package

--- app/src/main/java/com/example/cafemanagerapp/Activity/LoginActivity.java
+++ app/src/main/java/com/example/cafemanagerapp/Activity/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.example.cafemanagerapp;
+package com.example.cafemanagerapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.cafemanagerapp.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register_layout);
+        setContentView(R.layout.activity_login);
     }
 }
