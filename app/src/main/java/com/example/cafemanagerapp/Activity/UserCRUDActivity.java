500a12742e86ce406fb623f421415c31d8ca1bc7
NguyenHuyHoang230499 <103480136+NguyenHuyHoang230499@users.noreply.github.com>
2023-03-08 16:57:54 +0700

    testing userCRUD

--- app/src/main/java/com/example/cafemanagerapp/Activity/UserCRUDActivity.java
+++ app/src/main/java/com/example/cafemanagerapp/Activity/UserCRUDActivity.java
@@ -1,16 +1,44 @@
 package com.example.cafemanagerapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.LinearLayout;
 
+import com.example.cafemanagerapp.Adapter.UserCRUDAdapter;
+import com.example.cafemanagerapp.Entity.User;
 import com.example.cafemanagerapp.R;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserCRUDActivity extends AppCompatActivity {
+    private List<User> mListUser;
+    private UserCRUDAdapter userCRUDAdapter;
+    private RecyclerView rcvUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_crudactivity);
+        initUi();
+        mListUser = new ArrayList<>();
+        userCRUDAdapter = new UserCRUDAdapter();
+        LinearLayoutManager l = new LinearLayoutManager(this);
+        rcvUser.setLayoutManager(l);
+        rcvUser.setAdapter(userCRUDAdapter);
+        LoadData();
+    }
+    private void LoadData(){
+        mListUser.add(new User(1, "nguyen a", "abc", "123", "abc@gmail", "19001000",true,new Date(1/1/1999), true));
+        mListUser.add(new User(2, "nguyen b", "abcd", "1234", "abcd@gmail", "19001000",false,new Date(1/1/1999), false));
+        userCRUDAdapter.setData(mListUser);
+    }
+    private void initUi(){
+        rcvUser = findViewById(R.id.rcvUser);
     }
 }
