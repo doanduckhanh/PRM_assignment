1e520dd0ac1daa12424a52dd44e30c85d3f9e3e2
Nguyễn Tiến Nhất <nhatnthe153132@fpt.edu.vn>
2023-03-09 00:21:49 +0700

    update

--- app/src/main/java/com/example/cafemanagerapp/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/cafemanagerapp/Activity/RegisterActivity.java
@@ -10,35 +10,24 @@ import android.view.View;
 
 import com.example.cafemanagerapp.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
 
-
-    //Todo: go back wellcome
-    public void backFromLogin(View view){
+    public void backFromRegister(View view){
         Intent intent = new Intent(getApplicationContext(),WellcomeActivity.class);
         Pair[] pairs = new Pair[1];
-        pairs[0] = new Pair<View, String>(findViewById(R.id.layoutLogin),"transition_login");
+        pairs[0] = new Pair<View, String>(findViewById(R.id.layoutRegister),"transition_signup");
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this,pairs);
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(RegisterActivity.this,pairs);
             startActivity(intent,options.toBundle());
-        }
-        else {
+        }else {
             startActivity(intent);
         }
-    }
-
-    // Todo : go to register
-    public void callRegisterFromLogin(View view)
-    {
-        Intent intent = new Intent(getApplicationContext(),RegisterActivity.class);
-        startActivity(intent);
-        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
     }
 }
--- app/src/main/java/com/example/cafemanagerapp/Activity/WellcomeActivity.java
+++ app/src/main/java/com/example/cafemanagerapp/Activity/WellcomeActivity.java
@@ -2,7 +2,12 @@ package com.example.cafemanagerapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.cafemanagerapp.R;
 
@@ -12,5 +17,34 @@ public class WellcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wellcome);
+
+    }
+    // Todo: goto login
+    public void callLoginFromWel(View view){
+        Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+        Pair[] pairs = new Pair[1];
+        pairs[0] = new Pair<View, String>(findViewById(R.id.btn_login), "transition_login");
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(WellcomeActivity.this,pairs);
+            startActivity(intent,options.toBundle());
+        }
+        else {
+            startActivity(intent);
+        }
+    }
+
+    // Todo: goto register
+    public void callSignUpFromWel(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(),RegisterActivity.class);
+        Pair[] pairs = new Pair[1];
+        pairs[0] = new Pair<View, String>(findViewById(R.id.btn_signup),"transition_signup");
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(WellcomeActivity.this,pairs);
+            startActivity(intent,options.toBundle());
+        }
+        else {
+            startActivity(intent);
+        }
     }
 }
