2945215e29e9afe75b4dfc395a1b28146ce071f1
NguyenHuyHoang230499 <103480136+NguyenHuyHoang230499@users.noreply.github.com>
2023-03-08 11:04:30 +0700

    Add user CRUD activity

---
+++ app/src/main/java/com/example/cafemanagerapp/Activity/UserCRUDActivity.java
@@ -0,0 +1,16 @@
+package com.example.cafemanagerapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cafemanagerapp.R;
+
+public class UserCRUDActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_crudactivity);
+    }
+}
