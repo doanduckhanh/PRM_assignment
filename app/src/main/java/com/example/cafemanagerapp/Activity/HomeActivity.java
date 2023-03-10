52fd164c2e35a111c518f820a399aa8752671ae9
Nguyễn Tiến Nhất <nhatnthe153132@fpt.edu.vn>
2023-03-10 22:16:39 +0700

    update home done v2

--- app/src/main/java/com/example/cafemanagerapp/Activity/HomeActivity.java
+++ app/src/main/java/com/example/cafemanagerapp/Activity/HomeActivity.java
@@ -8,12 +8,14 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.cafemanagerapp.Fragments.DisplayHomeFragment;
 import com.example.cafemanagerapp.R;
@@ -26,6 +28,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     FragmentManager fragmentManager;
     TextView txt_menu_username;
     SharedPreferences sharedPreferences;
+    Boolean isAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +65,8 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         Intent intent = getIntent();
         String username = intent.getStringExtra("username");
         txt_menu_username.setText("Xin chào " + username + "!");
+        sharedPreferences = getSharedPreferences("role", Context.MODE_PRIVATE);
+        isAdmin = sharedPreferences.getBoolean("isAdmin", false);
 
 
         // Todo: set fragment home is default
@@ -84,6 +89,53 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                 tranDisplayHome.commit();
                 navigationView.setCheckedItem(item.getItemId());
                 drawerLayout.closeDrawers();
+                break;
+            case R.id.nav_statistic:
+//                FragmentTransaction tranDisplayStatistic = fragmentManager.beginTransaction();
+//                DisplayStatisticFragment displayStatisticFragment = new DisplayStatisticFragment();
+//                tranDisplayStatistic.replace(R.id.contentView,displayStatisticFragment);
+//                tranDisplayStatistic.commit();
+//                navigationView.setCheckedItem(item.getItemId());
+//                drawerLayout.closeDrawers();
+                break;
+
+            case R.id.nav_table:
+//                FragmentTransaction tranDisplayTable = fragmentManager.beginTransaction();
+//                DisplayTableFragment displayTableFragment = new DisplayTableFragment();
+//                tranDisplayTable.replace(R.id.contentView,displayTableFragment);
+//                tranDisplayTable.commit();
+//                navigationView.setCheckedItem(item.getItemId());
+//                drawerLayout.closeDrawers();
+                break;
+
+            case R.id.nav_category:
+//                FragmentTransaction tranDisplayMenu = fragmentManager.beginTransaction();
+//                DisplayCategoryFragment displayCategoryFragment = new DisplayCategoryFragment();
+//                tranDisplayMenu.replace(R.id.contentView, displayCategoryFragment);
+//                tranDisplayMenu.commit();
+//                navigationView.setCheckedItem(item.getItemId());
+//                drawerLayout.closeDrawers();
+//
+                 break;
+
+            case R.id.nav_staff:
+                if(isAdmin == true){
+//                    FragmentTransaction tranDisplayStaff = fragmentManager.beginTransaction();
+//                    DisplayStaffFragment displayStaffFragment = new DisplayStaffFragment();
+//                    tranDisplayStaff.replace(R.id.contentView,displayStaffFragment);
+//                    tranDisplayStaff.commit();
+//                    navigationView.setCheckedItem(item.getItemId());
+//                    drawerLayout.closeDrawers();
+                    Toast.makeText(getApplicationContext(),"Bạn có quyền truy cập",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getApplicationContext(),"Bạn không có quyền truy cập",Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+
+            case R.id.nav_logout:
+                Intent intent = new Intent(getApplicationContext(),WellcomeActivity.class);
+                startActivity(intent);
                 break;
         }
         return false;
