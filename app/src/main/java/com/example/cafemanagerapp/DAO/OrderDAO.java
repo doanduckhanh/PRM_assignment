d0423a8a2b4ecfca57017baf3786d4f021971ed4
khanhddhe150956 <khanhddhe150956@fpt.edu.vn>
2023-03-14 17:54:31 +0700

    Update OrderDAO.java

--- app/src/main/java/com/example/cafemanagerapp/DAO/OrderDAO.java
+++ app/src/main/java/com/example/cafemanagerapp/DAO/OrderDAO.java
@@ -1,7 +1,12 @@
 package com.example.cafemanagerapp.DAO;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+
+import com.example.cafemanagerapp.Entity.Order;
 
 @Dao
 public interface OrderDAO {
+    @Insert
+    void insert(Order order);
 }
