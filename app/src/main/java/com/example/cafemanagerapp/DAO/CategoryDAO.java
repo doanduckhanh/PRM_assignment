6726c1f043e240528cbb2377dbb3b7144dd509ee
khanhddhe150956 <khanhddhe150956@fpt.edu.vn>
2023-03-15 10:58:56 +0700

    Update CategoryDAO.java

--- app/src/main/java/com/example/cafemanagerapp/DAO/CategoryDAO.java
+++ app/src/main/java/com/example/cafemanagerapp/DAO/CategoryDAO.java
@@ -1,7 +1,17 @@
 package com.example.cafemanagerapp.DAO;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.cafemanagerapp.Entity.Category;
+
+import java.util.List;
 
 @Dao
 public interface CategoryDAO {
+    @Query("SELECT * FROM Category")
+    List<Category> getAll();
+    @Insert
+    void insert(Category category);
 }
