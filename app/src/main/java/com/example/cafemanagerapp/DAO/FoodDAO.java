ecf37c76ac7f260124a4e0ad1c71595b24c5f64f
khanhddhe150956 <khanhddhe150956@fpt.edu.vn>
2023-03-15 10:58:59 +0700

    Update FoodDAO.java

--- app/src/main/java/com/example/cafemanagerapp/DAO/FoodDAO.java
+++ app/src/main/java/com/example/cafemanagerapp/DAO/FoodDAO.java
@@ -1,7 +1,14 @@
 package com.example.cafemanagerapp.DAO;
 
 import androidx.room.Dao;
+import androidx.room.Query;
+
+import com.example.cafemanagerapp.Entity.Food;
+
+import java.util.List;
 
 @Dao
 public interface FoodDAO {
+    @Query("Select * from food where category_id like '%' || :cate_id || '%'")
+    List<Food> getByCateId(int cate_id);
 }
